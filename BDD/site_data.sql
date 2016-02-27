00d97fdef3ba2401a069eb0149e9922bd3d89d17
William Nong <nerdynekoninja@gmail.com>
2016-02-27 20:03:26 +0100

    Updated Database

--- BDD/site_data.sql
+++ BDD/site_data.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Sam 20 Février 2016 à 19:32
+-- Généré le :  Sam 27 Février 2016 à 20:02
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -173,20 +173,35 @@ INSERT INTO `parcours` (`ID_parcours`, `Nom`, `Description`, `Couleur`) VALUES
 DROP TABLE IF EXISTS `projets`;
 CREATE TABLE IF NOT EXISTS `projets` (
   `ID_projets` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `Nom` int(11) NOT NULL,
+  `Nom` varchar(50) NOT NULL,
   `Date` date NOT NULL,
   `Description` varchar(2000) NOT NULL,
   `Caractéristique` varchar(2000) NOT NULL,
   `Logiciel` varchar(2000) NOT NULL,
   `Matériel` varchar(2000) NOT NULL,
-  `Poids` varchar(2000) NOT NULL,
+  `Poids` int(40) NOT NULL,
   `Miniature` varchar(200) NOT NULL,
   `Fichier_Projet` varchar(200) NOT NULL,
   `Vidéo` varchar(200) NOT NULL,
   `ID_parcours` int(11) unsigned NOT NULL,
   PRIMARY KEY (`ID_projets`),
   KEY `ID_parcours` (`ID_parcours`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Contenu de la table `projets`
+--
+
+INSERT INTO `projets` (`ID_projets`, `Nom`, `Date`, `Description`, `Caractéristique`, `Logiciel`, `Matériel`, `Poids`, `Miniature`, `Fichier_Projet`, `Vidéo`, `ID_parcours`) VALUES
+(1, 'eeee', '2016-02-01', 'azedjgvfkza', '2.5D', 'Potato', 'Une Passoire', 2, '../Pictures/0b4f995a.jpg', 'aqdohziuqq', 'asdf', 1),
+(2, 'ASDF the Game', '2016-01-01', 'ASDF', 'ASDF', 'ASDF', 'ASDF', 4, '../Pictures/1d3878a9.jpg', 'ASDF', 'ASDF', 1),
+(3, 'Tequilla', '2016-02-29', 'zdqsdf', 'dqfezfd', 'qzefqzfd', 'zzsfqzef', 5, '../Pictures/c7d064d4.jpg', 'azsd', 'zefef', 2),
+(4, 'Derp', '2016-02-02', 'gd', 'b', 'qrfq', 'qesrggqqes', 5, '../Pictures/136c27e7.jpg', 'WEZ4ERRF', 'qqrzqersq', 1),
+(5, 'Derp 2', '2016-02-17', 'Derp''s revenge', 'zef', 'Unity', 'JH NBJH? ', 0, '../Pictures/apk0duvhadivsgm9htgfsqkavtfce0gv75apectsa7ivpnotldxefyjpayzkjk6rqoxybietolzo2hsb3f7leiw3sfaug_mk5ljjmdwnsi55bgrqbbuakzf8z8czmiih8.jpg', 'aaaasssssssss', 'rsrgfvere', 2),
+(6, 'Toto à la plage', '2016-02-27', 'EIUD', 'EQUFZQ', 'FCLJKQ', 'zefis', 4, '../Pictures/croatie-lacs-plitvice-cascades-7.jpg', 'rese', 'zqrgf', 2),
+(8, 'What''s 9+10', '2016-02-21', '21', '21', '21', '21', 2, '../Pictures/d6ec1e3e9f_seychelles-45.jpg', '21', '21', 3),
+(9, 'OOOOOOOOOOO', '2016-02-10', 'OOOOOOOOOOOOOOOOOOOOOOOO', 'OOOOOOOOOOOOOO', 'OOOOOOOOOOOOO', 'O', 0, '../Pictures/0ced1177.jpg', 'OOOOOOOOOOOOO', 'OOOOOOOOOOOOOOOOOOOOOOOOO', 3),
+(10, 'Derp Origins', '2016-03-16', 'Derp Begins', 'Real 4.5D', 'kjckcvj', 'Balançoire', 1337, '../Pictures/6812976929_6654ee46d1_b.jpg', 'AAAAAAAAAAAAAA', 'Andthisisthevideo', 3);
 
 -- --------------------------------------------------------
 
