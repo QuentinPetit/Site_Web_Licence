24065e29f2cce5ed2ad5e8ab001226e13961a248
William Nong <nerdynekoninja@gmail.com>
2016-02-27 15:28:22 +0100

    Integrated Bootstrap (Finally !) Set up "News" section and Carousel into one row (and made it all fabulously responsive)

--- Viewer/index.php
+++ Viewer/index.php
@@ -7,8 +7,10 @@
 		<link type="text/css" rel="stylesheet" href="../CSS/style.css">
 		<link rel="stylesheet" type="text/css" href="../Ressources/owl-carousel/owl.carousel.css">
 		<link rel="stylesheet" type="text/css" href="../Ressources/owl-carousel/owl.theme.css">
+		<link rel="stylesheet" type="text/css" href="../Ressources/bootstrap-3.3.6-dist/css/bootstrap.min.css">
 		<script type="text/javascript" src="../Ressources/owl-carousel/jquery-1.9.1.min.js"></script>
 		<script type="text/javascript" src="../Ressources/owl-carousel/owl.carousel.js"></script>
+		<script type="text/javascript" src="../Ressources/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function() {
 				$("#owl-example").owlCarousel({
@@ -79,40 +81,42 @@
 	</header>
 
 	<body>
-		<div class="actu">
-			<?php
-						
-				include('../PHP/connexion.php');
+		<div class="row">
+			<div class="actu col-xs-12 col-md-8 well">
+				<?php
+							
+					include('../PHP/connexion.php');
 
-				$sql = "SELECT Titre, Article FROM actualites ORDER BY DateCreation DESC LIMIT 2";
-				$result = $conn->query($sql);
+					$sql = "SELECT Titre, Article FROM actualites ORDER BY DateCreation DESC LIMIT 2";
+					$result = $conn->query($sql);
 
-				if ($result->num_rows > 0) {
-					// output data of each row
-					while($row = $result->fetch_assoc()) {
-					echo "<h2>". utf8_encode($row["Titre"]) ."</h2>";
-					echo "<article>". utf8_encode($row["Article"]) ."</article>";
+					if ($result->num_rows > 0) {
+						// output data of each row
+						while($row = $result->fetch_assoc()) {
+						echo "<h2>". utf8_encode($row["Titre"]) ."</h2>";
+						echo "<article>". utf8_encode($row["Article"]) ."</article>";
+						}
+					} else {
+						 echo "0 results";
 					}
-				} else {
-				     echo "0 results";
-				}
 
-				include('../PHP/deconnexion.php');
+					include('../PHP/deconnexion.php');
 
-			?>  
-		</div>
-		<div class="actu">
-			
-			<div id="owl-example" class="owl-carousel">
-				<div class="item"><img src="../Pictures/0b4f995a.jpg" alt="Owl Image"></div>
-				<div class="item"><img src="../Pictures/1d3878a9.jpg" alt="Owl Image"></div>
-				<div class="item"><img src="../Pictures/136c27e7.jpg" alt="Owl Image"></div>
-				<div class="item"><img src="../Pictures/apk0duvhadivsgm9htgfsqkavtfce0gv75apectsa7ivpnotldxefyjpayzkjk6rqoxybietolzo2hsb3f7leiw3sfaug_mk5ljjmdwnsi55bgrqbbuakzf8z8czmiih8.jpg" alt="Owl Image"></div>
-				<div class="item"><img src="../Pictures/c7d064d4.jpg" alt="Owl Image"></div>
-				<div class="item"><img src="../Pictures/croatie-lacs-plitvice-cascades-7.jpg" alt="Owl Image"></div>
-				<div class="item"><img src="../Pictures/d6ec1e3e9f_seychelles-45.jpg" alt="Owl Image"></div>
+				?>  
 			</div>
-			
+			<div class="actu col-xs-12 col-md-4">
+				
+				<div id="owl-example" class="owl-carousel">
+					<div class="item"><img src="../Pictures/0b4f995a.jpg" alt="Owl Image"></div>
+					<div class="item"><img src="../Pictures/1d3878a9.jpg" alt="Owl Image"></div>
+					<div class="item"><img src="../Pictures/136c27e7.jpg" alt="Owl Image"></div>
+					<div class="item"><img src="../Pictures/apk0duvhadivsgm9htgfsqkavtfce0gv75apectsa7ivpnotldxefyjpayzkjk6rqoxybietolzo2hsb3f7leiw3sfaug_mk5ljjmdwnsi55bgrqbbuakzf8z8czmiih8.jpg" alt="Owl Image"></div>
+					<div class="item"><img src="../Pictures/c7d064d4.jpg" alt="Owl Image"></div>
+					<div class="item"><img src="../Pictures/croatie-lacs-plitvice-cascades-7.jpg" alt="Owl Image"></div>
+					<div class="item"><img src="../Pictures/d6ec1e3e9f_seychelles-45.jpg" alt="Owl Image"></div>
+				</div>
+				
+			</div>
 		</div>
 	</body>
 
