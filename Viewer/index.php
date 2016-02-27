2458a10b239e9fb1d2809eb52a9a28299cbccd3e
QuentinPetit <quentin43@sfr.fr>
2016-02-27 15:53:17 +0100

    carousel with progress bar

--- Viewer/index.php
+++ Viewer/index.php
@@ -13,16 +13,105 @@
 		<script type="text/javascript" src="../Ressources/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function() {
-				$("#owl-example").owlCarousel({
+ 
+			  var time = 7; // time in seconds
+			 
+			  var $progressBar,
+			      $bar, 
+			      $elem, 
+			      isPause, 
+			      tick,
+			      percentTime;
+			 
+			    //Init the carousel
+			    $("#owl-example").owlCarousel({
+			      slideSpeed : 500,
+			      paginationSpeed : 500,
+			      singleItem : true,
+			      afterInit : progressBar,
+			      afterMove : moved,
+			      startDragging : pauseOnDragging
+			    });
+			 
+			    //Init progressBar where elem is $("#owl-example")
+			    function progressBar(elem){
+			      $elem = elem;
+			      //build progress bar elements
+			      buildProgressBar();
+			      //start counting
+			      start();
+			    }
+			 
+			    //create div#progressBar and div#bar then prepend to $("#owl-example")
+			    function buildProgressBar(){
+			      $progressBar = $("<div>",{
+			        id:"progressBar"
+			      });
+			      $bar = $("<div>",{
+			        id:"bar"
+			      });
+			      $progressBar.append($bar).prependTo($elem);
+			    }
+			 
+			    function start() {
+			      //reset timer
+			      percentTime = 0;
+			      isPause = false;
+			      //run interval every 0.01 second
+			      tick = setInterval(interval, 10);
+			    };
+			 
+			    function interval() {
+			      if(isPause === false){
+			        percentTime += 1 / time;
+			        $bar.css({
+			           width: percentTime+"%"
+			         });
+			        //if percentTime is equal or greater than 100
+			        if(percentTime >= 100){
+			          //slide to next item 
+			          $elem.trigger('owl.next')
+			        }
+			      }
+			    }
+			    //pause while dragging 
+			    function pauseOnDragging(){
+			      isPause = true;
+			    }
+			 
+			    //moved callback
+			    function moved(){
+			      //clear interval
+			      clearTimeout(tick);
+			      //start again
+			      start();
+			    }
+			    //uncomment this to make pause on mouseover 
+			    $elem.on('mouseover',function(){
+			      isPause = true;
+			    })
+			    $elem.on('mouseout',function(){
+			      isPause = false;
+			    })
+			 
+		});
 
-					//console.log("owlCarousel");
-					autoPlay: 3000, //Set AutoPlay to 3 seconds
 
-					items : 4,
-					itemsDesktop : [1199,3],
-					itemsDesktopSmall : [979,3]
-				});
-			});
+	    /*$(document).ready(function() {
+	     
+	      $("#owl-demo").owlCarousel({
+	     
+	          autoPlay: 3000, //Set AutoPlay to 3 seconds
+	     
+	          items : 4,
+	          itemsDesktop : [1199,3],
+	          itemsDesktopSmall : [979,3]
+	     
+	      });
+	     
+	    });*/
+
+
 		</script>
 	</head>
 
@@ -106,7 +195,7 @@
 			</div>
 			<div class="actu col-xs-12 col-md-4">
 				
-				<div id="owl-example" class="owl-carousel">
+				<div id="owl-example" class="owl-carousel owl-theme">
 					<div class="item"><img src="../Pictures/0b4f995a.jpg" alt="Owl Image"></div>
 					<div class="item"><img src="../Pictures/1d3878a9.jpg" alt="Owl Image"></div>
 					<div class="item"><img src="../Pictures/136c27e7.jpg" alt="Owl Image"></div>
