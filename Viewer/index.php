7db915c506c1179d06e49558a5648e5a0269ba1b
QuentinPetit <quentin43@sfr.fr>
2016-02-27 13:35:21 +0100

    owl-carousel

--- Viewer/index.php
+++ Viewer/index.php
@@ -5,9 +5,27 @@
 		<title>Licence Professionnelle Image & Son</title>
 		<meta charset="UTF-8"/>
 		<link type="text/css" rel="stylesheet" href="../CSS/style.css">
+		<link rel="stylesheet" type="text/css" href="../Ressources/owl-carousel/owl.carousel.css">
+		<link rel="stylesheet" type="text/css" href="../Ressources/owl-carousel/owl.theme.css">
+		<script type="text/javascript" src="../Ressources/owl-carousel/jquery-1.9.1.min.js"></script>
+		<script type="text/javascript" src="../Ressources/owl-carousel/owl.carousel.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$("#owl-demo").owlCarousel({
+
+					//console.log("owlCarousel");
+					autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+					items : 4,
+					itemsDesktop : [1199,3],
+					itemsDesktopSmall : [979,3]
+				});
+			});
+		</script>
 	</head>
 
 	<header>
+
 		<ul id="menu">
 			<li><a href="index.html">Accueil</a></li>
 			<li><a href="parcours.html">Parcours</a>
@@ -84,80 +102,17 @@
 			?>  
 		</div>
 		<div class="actu">
-
-						<div class="owl-wrapper-outer"><div style="width: 4180px; left: 0px; display: block; transition: all 800ms ease 0s; transform: translate3d(-418px, 0px, 0px);" class="owl-wrapper"><div style="width: 418px;" class="owl-item"><div class="item">	
-							<div class="blog-box2">
-								<figure>
-									<img src="http://img0.mxstatic.com/wallpapers/44e535006cffbc1b6e41f72d5e9df1e4_large.jpeg" alt="image" class="img-responsive">
-									<ul>
-										<li><i class="fa fa-calendar"></i> <span>Jan 3, 2016</span></li>
-										<li><i class="fa fa-comment"></i> <span>180</span></li>
-									</ul>
-								</figure>
-								<h4>Snowy Forest</h4>
-								<p> Lorem ipsum dolor sit amet Maecenas ullamcorper diam nec augue semper ...</p>		
-								<a href="blog-single.html" class="link">Read more...</a>	
-							</div>	
-						</div></div><div style="width: 418px;" class="owl-item"><div class="item">	
-							<div class="blog-box2">
-								<figure>
-									<img src="http://www.unesourisetmoi.info/wall32/images/paysage-fonds-ecran_04.jpg" alt="image" class="img-responsive">
-									<ul>
-										<li><i class="fa fa-calendar"></i> <span>Feb 16, 2016</span></li>
-										<li><i class="fa fa-comment"></i> <span>16</span></li>
-									</ul>
-								</figure>
-								<h4>Nice View</h4>
-								<p> Lorem ipsum dolor sit amet Maecenas ullamcorper diam nec augue semper ...</p>		
-								<a href="blog-single.html" class="link">Read more...</a>	
-							</div>	
-						</div></div><div style="width: 418px;" class="owl-item"><div class="item">	
-							<div class="blog-box2">
-								<figure>
-									<img src="http://img0.mxstatic.com/wallpapers/bfbd89b2ff0e40a77fe4fb5c31cc877a_large.jpeg" alt="image" class="img-responsive">
-									<ul>
-										<li><i class="fa fa-calendar"></i> <span>March 8, 2016</span></li>
-										<li><i class="fa fa-comment"></i> <span>280</span></li>
-									</ul>
-								</figure>
-								<h4>Cool Waves</h4>
-								<p> Lorem ipsum dolor sit amet Maecenas ullamcorper diam nec augue semper ...</p>		
-								<a href="blog-single.html" class="link">Read more...</a>	
-							</div>	
-						</div></div><div style="width: 418px;" class="owl-item"><div class="item">	
-							<div class="blog-box2">
-								<figure>
-									<img src="http://www.unesourisetmoi.info/data/images/photos/059/fonds-ecran-hd_paysages_09.jpg" alt="image" class="img-responsive">
-									<ul>
-										<li><i class="fa fa-calendar"></i> <span>April 11, 2016</span></li>
-										<li><i class="fa fa-comment"></i> <span>95</span></li>
-									</ul>
-								</figure>
-								<h4>Plane View</h4>
-								<p> Lorem ipsum dolor sit amet Maecenas ullamcorper diam nec augue semper ...</p>		
-								<a href="blog-single.html" class="link">Read more...</a>	
-							</div>	
-						</div></div><div style="width: 418px;" class="owl-item"><div class="item">	
-							<div class="blog-box2">
-								<figure>
-									<img src="http://a397.idata.over-blog.com/3/64/44/21/gif-ou-image/paysage-asiatique.jpg" alt="image" class="img-responsive">
-									<ul>
-										<li><i class="fa fa-calendar"></i> <span>May 18, 2016</span></li>
-										<li><i class="fa fa-comment"></i> <span>35</span></li>
-									</ul>
-								</figure>
-								<h4>Skie Rides</h4>
-								<p> Lorem ipsum dolor sit amet Maecenas ullamcorper diam nec augue semper ...</p>		
-								<a href="blog-single.html" class="link">Read more...</a>	
-							</div>	
-						</div></div></div></div>
-						
-						
-						
-						
-							
-
-					<div class="owl-controls clickable"><div class="owl-pagination"><div class="owl-page"><span class=""></span></div><div class="owl-page active"><span class=""></span></div></div></div>
+			
+			<div id="owl-example" class="owl-carousel">
+				<div class="item"><img src="../Pictures/0b4f995a.jpg" alt="Owl Image"></div>
+				<div class="item"><img src="../Pictures/1d3878a9.jpg" alt="Owl Image"></div>
+				<div class="item"><img src="../Pictures/136c27e7.jpg" alt="Owl Image"></div>
+				<div class="item"><img src="../Pictures/apk0duvhadivsgm9htgfsqkavtfce0gv75apectsa7ivpnotldxefyjpayzkjk6rqoxybietolzo2hsb3f7leiw3sfaug_mk5ljjmdwnsi55bgrqbbuakzf8z8czmiih8.jpg" alt="Owl Image"></div>
+				<div class="item"><img src="../Pictures/c7d064d4.jpg" alt="Owl Image"></div>
+				<div class="item"><img src="../Pictures/croatie-lacs-plitvice-cascades-7.jpg" alt="Owl Image"></div>
+				<div class="item"><img src="../Pictures/d6ec1e3e9f_seychelles-45.jpg" alt="Owl Image"></div>
+			</div>
+			
 		</div>
 	</body>
 
