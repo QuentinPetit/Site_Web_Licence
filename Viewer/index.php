aeb368768b532cdf3f124dff28627f5182129bee
William Nong <nerdynekoninja@gmail.com>
2016-02-27 17:20:56 +0100

    Changed menu bar with dropdowns to navbar with drop downs

--- Viewer/index.php
+++ Viewer/index.php
@@ -119,56 +119,63 @@
 			<img src="../Pictures/logo.png"/>
 			<img src="../Pictures/worker.jpg"/>
 		</div>
-		<ul id="menu">
-			<li><a href="index.html">Accueil</a></li>
-			<li><a href="parcours.html">Parcours</a>
-				<ul>
-					<?php
-
-						include('../PHP/connexion.php');
-
-						$sql = "SELECT Nom FROM parcours";
-						$result = $conn->query($sql);
-
-						if ($result->num_rows > 0) {
-							// output data of each row
-							while($row = $result->fetch_assoc()) {
-							echo "<li><a href='parcours.html'>". utf8_encode($row["Nom"]) ."</a></li>";
-							}
-						} else {
-						     echo "0 results";
-						}
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="index.php">Accueil</a></li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="parcours.html">Parcours</a>
 						
-						include('../PHP/deconnexion.php');
+						<ul class="dropdown-menu">
+							<?php
 
-					?>  
+								include('../PHP/connexion.php');
+
+								$sql = "SELECT Nom FROM parcours";
+								$result = $conn->query($sql);
+
+								if ($result->num_rows > 0) {
+									// output data of each row
+									while($row = $result->fetch_assoc()) {
+									echo "<li><a href='parcours.html'>". utf8_encode($row["Nom"]) ."</a></li>";
+									}
+								} else {
+									 echo "0 results";
+								}
+								
+								include('../PHP/deconnexion.php');
+
+							?>  
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="parcours.html">Projets</a>
+						<ul class="dropdown-menu">
+							<?php
+								
+								include('../PHP/connexion.php');
+
+								$sql = "SELECT Nom FROM parcours";
+								$result = $conn->query($sql);
+
+								if ($result->num_rows > 0) {
+									// output data of each row
+									while($row = $result->fetch_assoc()) {
+									echo "<li><a href='parcours.html'>". utf8_encode($row["Nom"]) ."</a></li>";
+									}
+								} else {
+									 echo "0 results";
+								}
+
+								include('../PHP/deconnexion.php');
+
+							?>  
+						</ul>
+					</li>
+					<li><a href='contact.html'>Contact</a></li>
 				</ul>
-			</li>
-			<li><a href="parcours.html">Projets</a>
-				<ul>
-					<?php
-						
-						include('../PHP/connexion.php');
-
-						$sql = "SELECT Nom FROM parcours";
-						$result = $conn->query($sql);
-
-						if ($result->num_rows > 0) {
-							// output data of each row
-							while($row = $result->fetch_assoc()) {
-							echo "<li><a href='parcours.html'>". utf8_encode($row["Nom"]) ."</a></li>";
-							}
-						} else {
-						     echo "0 results";
-						}
-
-						include('../PHP/deconnexion.php');
-
-					?>  
-				</ul>
-			</li>
-			<li><a href='contact.html'>Contact</a></li>
-		</ul>
+			</div>
+		</nav>
 	</header>
 
 	<body>
