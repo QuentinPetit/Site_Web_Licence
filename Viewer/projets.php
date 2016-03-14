ff946f66cd2ff084977bfa854cd9e5b6433e359c
Quentin Petit <quentin.petit43@gmail.com>
2016-03-14 18:08:06 +0100

    Sorted projects by year, discipline and weight

--- Viewer/projets.php
+++ Viewer/projets.php
@@ -21,11 +21,15 @@
 				<?php
 
 					$parcoursId=$_GET['parcoursId'];
-					$years;
+					$years; 
+					$matiere;
 					$init=false;
 					include('../PHP/connexion.php');
 
-					$sql="SELECT * FROM projets NATURAL JOIN anneescolaire WHERE ID_parcours='".$parcoursId."'ORDER BY DateFin DESC, Poids DESC";
+					$sql="SELECT projets.Nom AS NomProjet, matieres.Nom AS NomMatiere, anneescolaire.DateFin, anneescolaire.DateDebut, projets.Miniature, projets.Poids, projets.ID_projets FROM projets, matierestoprojet, matieres, anneescolaire WHERE projets.ID_Annee = anneescolaire.ID_Annee 
+						AND projets.ID_projets = matierestoprojet.ID_projets
+						AND matieres.ID_matieres = matierestoprojet.ID_matieres
+						AND ID_parcours='".$parcoursId."'ORDER BY DateFin DESC, NomMatiere, Poids DESC";
 					$result = $conn->query($sql);
 					if ($result->num_rows > 0) {
 						echo"<section class='col-xs-12 well'>";
@@ -33,7 +37,10 @@
 							if ($init==false) {
 								$EndYears = date("Y", strtotime($row["DateFin"]));
 								$StartYears = date("Y", strtotime($row["DateDebut"]));
+								$matiere=utf8_encode($row["NomMatiere"]);
 								echo "<h1>".$StartYears."-".$EndYears."</h1>";
+								echo "<section class='col-xs-12'>";
+								echo "<h2>".utf8_encode($row["NomMatiere"])."</h2>";
 								$init=true;
 							}
 							if(date("Y", strtotime($row["DateFin"]))!=$EndYears || date("Y", strtotime($row["DateDebut"]))!=$StartYears)
@@ -41,16 +48,27 @@
 								$StartYears = date("Y", strtotime($row["DateDebut"]));
 								$EndYears = date("Y", strtotime($row["DateFin"]));
 								echo "</section>";
+								echo "</section>";
 								echo"<section class='col-xs-12 well'>";
 								echo "<h1>".$StartYears."-".$EndYears."</h1>";
+								echo "<section class='col-xs-12'>";
+								echo "<h2>".utf8_encode($row["NomMatiere"])."</h2>";
+								$matiere=utf8_encode($row["NomMatiere"]);
 							}
 
+							if ($matiere!=utf8_encode($row["NomMatiere"]))
+							{
+								$matiere=utf8_encode($row["NomMatiere"]);
+								echo "</section>";
+								echo "<section class='col-xs-12'>";
+								echo "<h2>".utf8_encode($row["NomMatiere"])."</h2>";
+							}
 							echo "
 							<div class='picholder col-xs-4 col-sm-3 col-md-2'>
 									<img class='fancypics' src='".utf8_encode($row["Miniature"])."'>
 									<a href='projet.php?projetID=".utf8_encode($row["ID_projets"])."'>
 										<div class='overlay'>
-											<p class='text_box'>".utf8_encode($row["Nom"])."</p>
+											<p class='text_box'>".utf8_encode($row["NomProjet"])."</p>
 											<div class='star-rating'><input id='inner-rating' type='hidden' class='rating' data-readonly value='".$row["Poids"]."'></div>
 										</div>
 									</a>
