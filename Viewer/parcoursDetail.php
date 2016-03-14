c019277f357c54066cc1e2945d1c9fb11a64c67d
Quentin Petit <quentin.petit43@gmail.com>
2016-03-14 16:42:45 +0100

    Détail des parcours généré automatiquement

--- Viewer/parcoursDetail.php
+++ Viewer/parcoursDetail.php
@@ -19,6 +19,35 @@
 					include('../PHP/connexion.php');
 
 					$parcoursId=$_GET['parcoursId'];
+					$sql = "SELECT * FROM parcours WHERE parcours.ID_parcours =".$parcoursId; 
+					$result = $conn->query($sql);
+					if ($result->num_rows >0) {
+						while ($row = $result->fetch_assoc()) {
+							echo "<h1>".utf8_encode($row["Nom"])."</h1>";
+							echo "
+								<article>
+									<h2>Objectifs</h2>
+									<p>".utf8_encode($row["Objectifs"])."</p>
+								</article>";
+							echo "
+								<article>
+									<h2>Compétences visées</h2>
+									<p>".utf8_encode($row["Competences"])."</p>
+								</article>";
+							echo "
+								<article>
+									<h2>Logiciels utilisés</h2>
+									<p>".utf8_encode($row["Logiciels"])."</p>
+								</article>";
+							echo "
+								<article>
+									<h2>Admissions</h2>
+									<p>".utf8_encode($row["Admission"])."</p>
+								</article>";
+						}
+					} else {
+						echo "0 results";
+					}
 
 					include('../PHP/deconnexion.php');
 
