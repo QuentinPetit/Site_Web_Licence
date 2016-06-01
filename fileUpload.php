71fe995b726590535524555c2e243662be91367c
Quentin Petit <quentin.petit43@gmail.com>
2016-06-01 10:50:54 +0200

    saisi projet

--- fileUpload.php
+++ fileUpload.php
@@ -5,9 +5,19 @@
   <meta charset="utf-8">
 </head>
 <body>
+  <<?php include('./PHP/connexion.php'); ?>
   <form action="fileUpload.php" method="POST" enctype="multipart/form-data">
-    <input type="file" name="userfile"><input type="submit" name="submit" value="Extract">
+    <h1>Ajouter un projet</h1>
+    <textarea name="nomProjets" maxlength="50" placeholder="Nom du projet"></textarea>
+    <textarea name="description" placeholder="Description du projet (synopsis, context...)"></textarea>
+    <textarea name="caractéristiques" placeholder="Caractéristiques du projet : toute technique spécifiques au projet (lancer de rayon, shader, modélisation low-poly...)"></textarea>
+    <select>
+      <option disabled="true">Année Scolaire</option>
+      <<?php  ?>
+    </select>
+    <input type="file" name="userfile"><input type="submit" name="submit" value="Ajouter">
   </form>
+  <<?php include('./PHP/deconnexion.php'); ?>
   <?php
     if (isset($_POST['submit'])) {
       $array = explode(".", $_FILES["userfile"]["name"]);
