f9b0e7b5f49392dc59300275be33d563e8468c0c
Quentin Petit <quentin.petit43@gmail.com>
2016-06-08 17:25:34 +0200

    Started AjouterParcours.php

--- Administration/ajoutParcours.php
+++ Administration/ajoutParcours.php
@@ -17,5 +17,82 @@
 	<?php include('../PHP/navbarAdmin.php') ?>
 </header>
 <body>
+	<?php include('../PHP/connexion.php'); ?>
+	<?php 
+	if (isset($_POST['submit']) &&
+		isset($_POST['nomParcours']) &&
+		isset($_POST['description']) &&
+		isset($_POST['objectifs']) &&
+		isset($_POST['competences']) &&
+		isset($_POST['logiciels']) &&
+		isset($_POST['admission']) 
+		) 
+	{
+		if ($_POST['nomParcours']!="" &&
+			$_POST['description']!="" &&
+			$_POST['objectifs']!="" &&
+			$_POST['competences']!="" &&
+			$_POST['logiciels']!="" &&
+			$_POST['admission']!="" &&) 
+		{
+			
+		}
+	}
+	?>
+	<div class="container-fluid">
+		<div class="col-xs-12 col-md-8 col-md-offset-2">
+			<form action="ajoutParcours.php" method="POST" enctype="multipart/form-data">
+				<h1>Ajouter un parcours</h1>
+				<?php
+				if (isset($error))
+				{
+					echo "<div class='alert alert-danger'>".$error."</div>"; 
+				}
+				if (isset($message)) 
+				{
+					echo "<div class='alert alert-success'>".$message."</div>";
+				}
+				?>
+				<div class="form-group">
+					<label>Nom du parcours</label>
+					<textarea class="form-control" placeholder="Nom" maxlength="40" name="nomParcours" rows="1"></textarea>
+				</div>
+				<div class="form-group">
+					<label>Description</label>
+					<textarea class="form-control" placeholder="Description du parcours." name="description" rows="5"></textarea>
+				</div>
+				<div class="form-group">
+					<label>Objectifs</label>
+					<textarea class="form-control" placeholder="Objectifs du parcours." name="objectifs" rows="5"></textarea>
+				</div>
+				<div class="form-group">
+					<label>Compétences</label>
+					<textarea class="form-control" placeholder="Compétences visées" name="competences" rows="5"></textarea>
+				</div>
+				<div class="form-group">
+					<label>Logiciels</label>
+					<textarea class="form-control" placeholder="Logiciels utilisés" name="logiciels" rows="5"></textarea>
+				</div>
+				<div class="form-group">
+					<label>Admission</label>
+					<textarea class="form-control" placeholder="Conditions d'admission au parcours." name="admission" rows="5"></textarea>
+				</div>
+				<div class="form-group">
+					<label>Plaquette</label>
+					<input type="file" class="form-control" name="plaquette"></input>
+				</div>
+				<div class="form-group">
+					<label>Mascotte</label>
+					<input type="file" class="form-control" name="mascotte"></input>
+				</div>
+				<div class="form-group">
+					<label>Image de fond</label>
+					<input type="file" class="form-control" name="fond"></input>
+				</div>
+				<button type="submit" class="btn btn-primary" name="submit">Ajouter</button>
+			</form>
+		</div>
+	</div>
+	<?php include('../PHP/deconnexion.php'); ?>
 </body>
 </html>
