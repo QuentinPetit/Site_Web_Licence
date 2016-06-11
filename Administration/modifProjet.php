8d0e76229e533860e49135e46be4bfcbd6cafbe0
QuentinPetit <quentin43@sfr.fr>
2016-06-11 15:39:27 +0200

    modifProjets.php update

--- Administration/modifProjet.php
+++ Administration/modifProjet.php
@@ -18,7 +18,43 @@
 </header>
 <body>
 	<?php include('../PHP/connexion.php'); ?>
+	<?php  
+	print_r($_POST);
+		if (isset($_POST['submit']) && 
+			isset($_POST['nomProjets']) && 
+			isset($_POST['description']) && 
+			isset($_POST['caracteristiques']) && 
+			isset($_POST['anneeScolaireSelect']) && 
+			isset($_POST['parcoursSelect']) && 
+			isset($_POST['participantsSelect']) && 
+			isset($_POST['site']) && 
+			isset($_POST['software']) && 
+			isset($_POST['hardware']) && 
+			isset($_POST['matiereSelect']) ) {
+			echo "toto";
+			if ($_POST['nomProjets'] != "" && 
+				$_POST['description'] != "" && 
+				$_POST['caracteristiques'] != "" && 
+				count($_POST['parcoursSelect'])>0 && 
+				ount($_POST['participantsSelect'])>0 && 
+				$_POST['software'] !="" && 
+				$_POST['hardware'] !="" &&
+				count($_POST['matiereSelect'])>0) {
 
+					$nomProjets=$_POST['nomProjets'];
+					$description=$_POST['description'];
+					$caracteristiques=$_POST['caracteristiques'];
+					$anneeScolaireSelect=$_POST['anneeScolaireSelect'];
+					$parcoursSelect=$_POST['parcoursSelect'];
+					$participantsSelect=$_POST['participantsSelect'];
+					$matiereSelect=$_POST['matiereSelect'];
+					$software=$_POST['software'];
+					$hardware=$_POST['hardware'];
+					$idData=$_POST['idData'];
+
+			}
+		}
+	?>
 	<div class="container-fluid">
 		<div class="col-xs-12 col-md-8 col-md-offset-2">
 			<form action="modifProjet.php?projetID=<?php echo $_GET['projetID'] ?>" method="POST" enctype="multipart/form-data">
@@ -45,9 +81,6 @@
 				<?php
 				}
 				?>				
-				
-				
-
 				<div class="form-group">
 					<label>Description</label>
 					<textarea class="form-control" name="description" placeholder="Description du projet (synopsis, context...)" rows="5" wrap="hard"><?php echo utf8_encode($row['Description']); ?></textarea>					
@@ -63,13 +96,31 @@
 					<select class="form-control" name="parcoursSelect[]" multiple="true">
 						<option disabled="true">Parcours</option>
 						<?php 
-						$sql = "SELECT * FROM parcours";
-						$result = $conn->query($sql);
-						if ($result->num_rows > 0) {
-							while ($row = $result->fetch_assoc()) {
-
-								echo"<option value=".$row["Dossier"].">".utf8_encode($row["Nom"])."</option>";
-
+						$sqlParcours = "SELECT * FROM parcours";
+						$resultParcours = $conn->query($sqlParcours);
+						$sqlProjetsToParcours = "SELECT ID_parcours FROM projetstoparcours WHERE ID_projets=".$_GET['projetID'];
+						$resultProjetsToParcours = $conn->query($sqlProjetsToParcours);
+						$arrayIDs = array();
+						if ($resultProjetsToParcours->num_rows > 0) {
+							while ($rowProjetsToParcours = $resultProjetsToParcours->fetch_assoc()) {
+								array_push($arrayIDs, $rowProjetsToParcours['ID_parcours']);
+							}
+						} else {
+							echo "0 results";
+						}
+						if ($resultParcours->num_rows > 0) {
+							while ($rowParcours = $resultParcours->fetch_assoc()) {
+								$trouver=false;
+								foreach ($arrayIDs as $arrayID) {
+									if ($arrayID==$rowParcours['ID_parcours']) {
+										$trouver=true;
+									}
+								}
+								if ($trouver==true) {
+									echo"<option value=".$rowParcours["Dossier"]." selected='true'>".utf8_encode($rowParcours["Nom"])."</option>";
+								} else {
+									echo"<option value=".$rowParcours["Dossier"]." >".utf8_encode($rowParcours["Nom"])."</option>";
+								}
 							}
 						} else {
 							echo "0 results";
@@ -96,31 +147,18 @@
 						}
 						if ($resultEleves->num_rows > 0) {
 							while ($rowEleves = $resultEleves->fetch_assoc()) {
-								$lastID;
-								$init = false;
+								$trouver=false;
 								foreach ($arrayIDs as $arrayID) {
-									if ($init==false){
-										if ($arrayID == $rowEleves["ID_eleves"]) {
-											echo"<option value=".$rowEleves["ID_eleves"]." selected='true'>".utf8_encode($rowEleves["Nom"])." ".utf8_encode($rowEleves["Prenom"])." </option>";
-										} else {
-											echo"<option value=".$rowEleves["ID_eleves"].">".utf8_encode($rowEleves["Nom"])." ".utf8_encode($rowEleves["Prenom"])."</option>";
-										}
-										$init=true;
-										$lastID=$rowEleves["ID_eleves"];
-									} else {
-										if ($lastID!=$rowEleves["ID_eleves"]) {
-											if ($arrayID == $rowEleves["ID_eleves"]) {
-												echo"<option value=".$rowEleves["ID_eleves"]." selected='true'>".utf8_encode($rowEleves["Nom"])." ".utf8_encode($rowEleves["Prenom"])." </option>";
-											} else {
-												echo"<option value=".$rowEleves["ID_eleves"].">".utf8_encode($rowEleves["Nom"])." ".utf8_encode($rowEleves["Prenom"])."</option>";
-											}
-											$lastID=$rowEleves["ID_eleves"];
-										}
-										
+									if ($arrayID==$rowEleves['ID_eleves']) {
+										$trouver=true;
 									}
-									
-									
 								}
+								if ($trouver==true) {
+									echo"<option value=".$rowEleves["ID_eleves"]." selected='true'>".utf8_encode($rowEleves["Nom"])." ".utf8_encode($rowEleves["Prenom"])." </option>";
+								} else {
+									echo"<option value=".$rowEleves["ID_eleves"].">".utf8_encode($rowEleves["Nom"])." ".utf8_encode($rowEleves["Prenom"])."</option>";
+								}
+								
 							}
 						} else {
 							echo "0 results";
@@ -133,12 +171,31 @@
 					<select class="form-control" name="matiereSelect[]" multiple="true">
 						<option disabled="true">Matières</option>
 						<?php 
-						$sql = "SELECT * FROM matieres ORDER BY Nom";
-						$result = $conn->query($sql);
-
-						if ($result->num_rows > 0) {
-							while ($row = $result->fetch_assoc()) {
-								echo"<option value=".$row["ID_matieres"].">".utf8_encode($row["Nom"])."</option>";
+						$sqlMatieres = "SELECT * FROM matieres ORDER BY Nom";
+						$resultMatieres = $conn->query($sqlMatieres);
+						$sqlMatieresToProjet = "SELECT ID_matieres FROM matierestoprojet WHERE ID_projets=".$_GET['projetID'];
+						$resultMatieresToProjet = $conn->query($sqlMatieresToProjet);
+						$arrayIDs = array();
+						if ($resultMatieresToProjet->num_rows > 0) {
+							while ($rowMatieresToProjet = $resultMatieresToProjet->fetch_assoc()) {
+								array_push($arrayIDs, $rowMatieresToProjet['ID_matieres']);
+							}
+						} else {
+							echo "0 results";
+						}
+						if ($resultMatieres->num_rows > 0) {
+							while ($rowMatieres = $resultMatieres->fetch_assoc()) {
+								$trouver=false;
+								foreach ($arrayIDs as $arrayID) {
+									if ($arrayID==$rowMatieres['ID_matieres']) {
+										$trouver=true;
+									}
+								}
+								if ($trouver==true) {
+									echo"<option value=".$rowMatieres["ID_matieres"]." selected='true'>".utf8_encode($rowMatieres["Nom"])."</option>";
+								} else {
+									echo"<option value=".$rowMatieres["ID_matieres"].">".utf8_encode($rowMatieres["Nom"])."</option>";
+								}
 							}
 						} else {
 							echo "0 results";
@@ -148,7 +205,24 @@
 				</div>
 				<div class="form-group">
 					<label>Fichier à uploader (.zip)</label>
-					<input type="file" class="form-control" name="userfile">					
+					</br>
+					<?php  
+						$sqlData = "SELECT * FROM data WHERE ID_Projets=".$_GET['projetID'];
+						$resultData = $conn->query($sqlData);
+						if ($resultData->num_rows>0) {
+							while ($rowData = $resultData->fetch_assoc()) {
+								$array = explode("/", utf8_encode($rowData["Lien"]));
+								if($rowData["ID_type"]!=3)
+								{
+									echo "<input type='checkbox' name='idData' value=".$rowData["ID_data"]." checked='true'>".end($array)."</input> </br>";
+								}
+							}
+						} else {
+							echo "0 result";
+						}
+						
+					?>
+					<input type="file" class="form-control" name="userfile">				
 				</div>
 				<div class="form-group">
 					<label>Lien du site de présentation</label>
@@ -166,12 +240,12 @@
 				if($user_statut=="enseignant") {
 					?> 
 					<label>Poids</label>
-					<input type="number" class="form-control" name="poids" min="0" max="100" value="50"><?php echo utf8_encode($row['Poids']); ?></input>     		
+					<input type="number" class="form-control" name="poids" min="0" max="100" value="<?php echo utf8_encode($row['Poids']); ?>"></input>     		
 					<?php 
 				}
 				?>
 
-				<button type="submit" class="btn btn-primary" name="submit">Ajouter</button>
+				<button type="submit" class="btn btn-primary" name="submit">Modifier</button>
 			</form>        
 		</div>
 
